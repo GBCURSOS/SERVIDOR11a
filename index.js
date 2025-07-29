18359ae562c2670575f287685656e740eda7efa6
rnogalegb <rnogales.docencia@gmail.com>
2025-07-28 21:27:15 -0500

    update listarUsuarios

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
-
 import express from 'express';
 import cors from 'cors';
-import { conectarDB } from './CONFIGDB/connectDB.js';
+import { conectoDB  } from './CONFIGDB/conectoDB.js';
 import { insertarSemillaDB } from './CONFIGDB/insertSemillaDB.js';
 import apiRoutes from './ROUTES/apiRoutes.js';
 
-import dotenv from 'dotenv';
-dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 4200;
+const PORT = 4100;
 
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 app.use(express.static("PUBLIC"));
 
 app.get('/prueba', (req, res) => {
-  res.json({message:'Prueba de respuesta de servidor'});
+    res.json({message:'Prueba de Respuesta de Servidor'})
 });
 
-conectarDB();
+conectoDB();
 insertarSemillaDB();
 
-// Usar las rutas de la API bajo el prefijo /api
 app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Para hombres: https://randomuser.me/api/portraits/men/{n}.jpg
